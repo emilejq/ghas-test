e7cd4b56efd349bc736132d79a30d19a2c11e87c
Emile <emile.el-qawas@santander.co.uk>
2022-04-27 14:48:17 +0100

    Update

--- src/main/java/com.test/controller/WebController.java
+++ src/main/java/com.test/controller/WebController.java
@@ -4,11 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,49 +17,49 @@ import java.net.URL;
 public class WebController {
 
   @GetMapping(path = "/login")
-  public @ResponseBody ResponseEntity<String> login(@RequestParam(name = "username") String username) {
-    return ResponseEntity.ok(String.format("Welcome back %s!", username));
+  public @ResponseBody ResponseEntity<String> login() {
+    return ResponseEntity.ok(String.format("Welcome back %s!", "username"));
   }
 
-  @GetMapping(path = "/picture/{user}")
-  public @ResponseBody ResponseEntity<FileInputStream> getImage(@PathVariable(name = "user") String user,
-      @RequestParam(name = "location", required = false) String location) {
-    try {
-      FileInputStream stream = null;
-
-      if (location != null) {
-        try {
-          URL url = new URL(location);
-          HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-          connection.setRequestMethod("GET");
-          stream = (FileInputStream) connection.getInputStream();
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      } else {
-        File file = new File("resources/images/", user);
-        stream = new FileInputStream(file);
-      }
-      return ResponseEntity.ok().body(stream);
-    } catch (FileNotFoundException e) {
-      return ResponseEntity.notFound().build();
-    }
-  }
-
-  @PostMapping(path = "/save-details")
-  public @ResponseBody ResponseEntity<Void> saveUserDetails(@RequestParam(name = "user_path") String path) {
-
-    try {
-      String command = "cmd.exe /c dir " + path;
-      if (command.contains("cmd.exe /c dir ")) {
-        Process process = Runtime.getRuntime().exec(command);
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        log.info(stdInput.readLine());
-      }
-    }
-    catch (IOException e) {
-      System.out.println("Error executing command");
-    }
-    return ResponseEntity.ok().build();
-  }
+//  @GetMapping(path = "/picture/{user}")
+//  public @ResponseBody ResponseEntity<FileInputStream> getImage(@PathVariable(name = "user") String user,
+//      @RequestParam(name = "location", required = false) String location) {
+//    try {
+//      FileInputStream stream = null;
+//
+//      if (location != null) {
+//        try {
+//          URL url = new URL(location);
+//          HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//          connection.setRequestMethod("GET");
+//          stream = (FileInputStream) connection.getInputStream();
+//        } catch (Exception e) {
+//          e.printStackTrace();
+//        }
+//      } else {
+//        File file = new File("resources/images/", user);
+//        stream = new FileInputStream(file);
+//      }
+//      return ResponseEntity.ok().body(stream);
+//    } catch (FileNotFoundException e) {
+//      return ResponseEntity.notFound().build();
+//    }
+//  }
+//
+//  @PostMapping(path = "/save-details")
+//  public @ResponseBody ResponseEntity<Void> saveUserDetails(@RequestParam(name = "user_path") String path) {
+//
+//    try {
+//      String command = "cmd.exe /c dir " + path;
+//      if (command.contains("cmd.exe /c dir ")) {
+//        Process process = Runtime.getRuntime().exec(command);
+//        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//        log.info(stdInput.readLine());
+//      }
+//    }
+//    catch (IOException e) {
+//      System.out.println("Error executing command");
+//    }
+//    return ResponseEntity.ok().build();
+//  }
 }
